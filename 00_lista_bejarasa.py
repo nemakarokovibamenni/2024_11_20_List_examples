5520c89e0f16adb81a26ad7499717b73e69f33ea
nemakarokovibamenni <barsony.gergely@diak.szbi-pg.hu>
2024-11-20 13:24:32 +0100

    Update 00_lista_bejarasa.py

--- 00_lista_bejarasa.py
+++ 00_lista_bejarasa.py
@@ -1,19 +1,33 @@
 # gyümölcsök kiíratása
 gyumolcsok = ['alma', 'körte', 'szilva', 'barack']
 
-print()
+print(gyumolcsok)
+for gyumolcs in gyumolcsok:
+    print(gyumolcs)
 
 # hónapok kiíratása
 honapok = ['január', 'február','március', 'április', 'május', 'június', 'július'] 
 
+for honap in honapok:
+    print(honap)
 print()
 
 # index megjelenítése:
     # 0 január
     # 1 február
 
+honap_index = 0
+for honap in honapok:
+    print(f"{honap_index} {honap}")
+    honap_index += 1
 print()
 
 # index felhasználása sorszámok megadásához:
     # 1. január
     # 2. február
+
+honap_index = 1
+for honap in honapok:
+    print(f"{honap_index}. {honap}")
+    honap_index += 1
+print(f"---🗓️Ezek a hónapok🗓️---")
