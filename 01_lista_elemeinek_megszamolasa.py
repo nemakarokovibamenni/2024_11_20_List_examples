bb27db93dc666cc940cad88798ce2d6e3e3d4c6a
nemakarokovibamenni <barsony.gergely@diak.szbi-pg.hu>
2024-11-20 13:40:26 +0100

    add 01 and 02 exercise solution

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,7 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+
+szamoklista = [5, 8, 12, 15, 22]
+print(len(szamoklista))
+?
